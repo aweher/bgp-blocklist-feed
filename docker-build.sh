6ae591210e52cf9182ea102acbd1e3201288801e
Ariel Weher <ariel@weher.net>
2022-05-23 13:08:29 -0300

    changed docker hub repo name

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PROYECTO="aweher/exabgp-blocklists"
+PROYECTO="aweher/bgp-blocklist-feed"
 NOW=$(date +%Y%m%d%H%M%S)
 docker build --no-cache -t $PROYECTO:$NOW -t $PROYECTO:latest .
 docker push $PROYECTO:$NOW
